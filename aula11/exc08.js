b79ad4e524418f3a2a45c9658a3b2f17e21dc177
filipe.nascimento1 <filipesn2006@gmail.com>
2024-11-05 07:57:30 -0300

    b

---
+++ aula11/exc08.js
@@ -0,0 +1,8 @@
+var vel = 59
+var max = 60
+console.log(`A velocidade do seu carro é: ${vel}Km/h`)
+if (vel >= max) {
+  console.log('Você ultrapassou a velocidade permitida, MUTADO!')
+}else{
+  console.log('Dirija com Cuidado!')
+}
